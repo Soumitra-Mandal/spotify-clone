13115fe2212e69815dc42808f1b8bcb585cc1e1d
Soumitra Mandal <soumitramandal1999@gmail.com>
2021-02-17 19:31:35 +0530

    add

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign,Ionicons,FontAwesome5  } from '@expo/vector-icons'; 
+
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+
+//import { AppearanceProvider,Appearance } from 'react-native-appearance';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SearchScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Search!</Text>
+    </View>
+  );
+}
+
+function LibraryScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Library!</Text>
+    </View>
+  );
+}
+function PremiumScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Premium!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+ /* console.log(Appearance.getColorScheme())*/
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //<AppearanceProvider>
+    <NavigationContainer >
+      <Tab.Navigator
+       screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home';
+              return <AntDesign name={iconName} size={size} color={color} />;
+
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search1' : 'search1';
+              return <AntDesign name={iconName} size={size} color={color} />;
+
+            }
+            else if (route.name === 'Library') {
+              iconName = focused ? 'library-sharp' : 'library-sharp';
+              return <Ionicons name={iconName} size={size} color={color} />;
+
+            }
+            else if (route.name === 'Premium') {
+              iconName = focused ? 'spotify' : 'spotify';
+              return <FontAwesome5  name={iconName} size={size} color={color} />;
+
+            }
+          },
+        })}
+      tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: 'gray',
+        }}
+      >
+
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Library" component={LibraryScreen} />
+        <Tab.Screen name="Premium" component={PremiumScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+    //</AppearanceProvider>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop:40,
+    flexDirection:'row',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',    
+  },
+  text:{
+    fontSize: 20,
+    padding:30,
+    textAlign: 'center',
+
   },
 });
